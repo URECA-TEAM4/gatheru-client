fa1bface603ea0fee401e7cf76109f94e426ebb4
JUJEOP <dapoo69@naver.com>
2024-09-19 11:37:43 +0900

    PostList.js 파일 추가

---
+++ src/components/views/MainPage/PostList.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react'
+import Post from './Post'
+import axios from 'axios'
+
+function PostList(props) {
+  const [posts, setPosts] = useState([])
+  let endpoints = ['/api/mogakos/get', '/api/studyContests/get']
+
+  function pastDeadline(post) {
+    let datetime = post.type == 'mogako' ? post.datetime : post.deadline
+    return Date.now() > new Date(datetime)
+  }
+
+  function beforeDeadline(post) {
+    let datetime = post.type == 'mogako' ? post.datetime : post.deadline
+    return Date.now() < new Date(datetime)
+  }
+
+  useEffect(() => {
+    axios
+      .all(endpoints.map(endpoint => axios.get(endpoint)))
+
+      .then(function (response) {
+        let combinedPosts = response[0].data.concat(response[1].data)
+        if (props.sorting == '최신순')
+          combinedPosts.sort(
+            (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+          )
+        else
+          combinedPosts.sort(
+            (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+          )
+
+        if (props.pastDeadline == 'all') {
+          setPosts(combinedPosts)
+        } else if (props.pastDeadline) {
+          setPosts(combinedPosts.filter(pastDeadline))
+        } else {
+          setPosts(combinedPosts.filter(beforeDeadline))
+        }
+      })
+
+      .catch(function (error) {
+        console.log(error)
+      })
+  }, [props.sorting])
+
+  return (
+    <>
+      {posts.map(post => {
+        return (
+          <Post
+            key={post.title}
+            postType={post.type}
+            title={post.title}
+            content={post.content}
+            location={post.location}
+            datetime={post.type == 'mogako' ? post.datetime : post.deadline}
+            method={post.type != 'mogako' ? post.method : ''}
+          />
+        )
+      })}
+    </>
+  )
+}
+
+export default PostList
