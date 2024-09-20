d326e11b8d6a4c30820847e3142d25fa40a13716
kimdayeon37 <kdy37912@naver.com>
2024-09-21 01:11:27 +0900

    feat: 모각코 캘린더 완성

--- src/components/views/CalendarPage/CalendarPage.js
+++ src/components/views/CalendarPage/CalendarPage.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Tab from '../../common/Tabs/Tab'
 import { Container } from '@mui/material'
-import {
-  mute_navy_color,
-  primary_color,
-  secondary_color,
-} from '../../constants/colors'
-// 캘린더 플러그인
+import { mute_navy_color } from '../../constants/colors'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
+import interactionPlugin from '@fullcalendar/interaction'
 
-import Auth from "../../../hoc/auth"
+import Auth from '../../../hoc/auth'
 
 function CalendarPage() {
-  const handleDateClick = arg => {
-    alert(arg.dateStr)
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await axios.get('/api/mogakos/get')
+      setEvents(response.data)
+    }
+    fetchEvents()
+  }, [])
+
+  const handleEventClick = info => {
+    const eventId = info.event.id // 클릭한 게시글 ID
+    const eventType = info.event.extendedProps.type // 클릭한 게시글 type
+    window.location.href = `/detail/${eventType}/${eventId}`
   }
 
   return (
@@ -25,30 +33,19 @@ function CalendarPage() {
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
-          events={[
-            {
-              title: '강남 모각코',
-              date: '2024-09-01T17:00',
-              color: mute_navy_color,
-            },
-            {
-              title: '스터디 모집',
-              start: '2024-09-02',
-              end: '2024-09-05',
-              color: secondary_color,
-            },
-            {
-              title: '공모전 모집',
-              start: '2024-09-04',
-              end: '2024-09-06',
-              color: primary_color,
-            },
-          ]}
-          dateClick={handleDateClick}
+          events={events.map(event => ({
+            id: event._id,
+            title: event.title,
+            date: event.datetime,
+            color: mute_navy_color,
+            extendedProps: { type: event.type }, // type을 extendedProps에 추가
+          }))}
+          eventClick={handleEventClick} // 이벤트 클릭 핸들러 설정
+          timeZone="UTC"
         />
       </Container>
     </div>
   )
 }
 
-export default Auth(CalendarPage, true);
+export default Auth(CalendarPage, true)
