a83229758c9a37666b2759915d5309e8a6f718f6
kimdayeon37 <kdy37912@naver.com>
2024-09-26 23:45:17 +0900

    fix: datetime picker으로 변경

--- src/components/views/NewPostPage/NewPostPage.js
+++ src/components/views/NewPostPage/NewPostPage.js
@@ -1,66 +1,78 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { 
-  TextField, 
-  Container, 
-  Button, 
-  Radio, 
-  RadioGroup, 
-  FormControlLabel, 
-  FormControl, 
-  FormLabel, 
-  Select, 
-  MenuItem, 
-  Grid 
-} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Auth from "../../../hoc/auth";
+import React, { useState } from 'react'
+import axios from 'axios'
+import {
+  TextField,
+  Container,
+  Button,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Select,
+  MenuItem,
+  Grid,
+} from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Auth from '../../../hoc/auth'
+
+import dayjs from 'dayjs'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 
 function NewPostPage() {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [deadline, setDeadline] = useState('');
-  const [maxParticipants, setMaxParticipants] = useState(1);
-  const [purpose, setPurpose] = useState('');
-  const [meetingType, setMeetingType] = useState('');
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [deadline, setDeadline] = useState(dayjs(''))
+  const [maxParticipants, setMaxParticipants] = useState(1)
+  const [purpose, setPurpose] = useState('')
+  const [meetingType, setMeetingType] = useState('')
 
-  const navigate = useNavigate();
-  const user = useSelector(state => state.user);
+  const navigate = useNavigate()
+  const user = useSelector(state => state.user)
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleDeadlineChange = (newValue) => {
+    setDeadline(newValue); // DateTimePicker에서 선택된 값을 상태에 저장
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault()
 
     const registrationData = {
       title,
       writer: user.userData._id,
       content: description,
-      deadline,
+      deadline: deadline.toISOString(),
       maximumNum: maxParticipants,
       type: purpose,
       method: meetingType,
-    };
+    }
 
     try {
-      const response = await axios.post('/api/studycontests/add', registrationData);
-      console.log('등록 성공:', response.data);
-      navigate('/');
+      const response = await axios.post(
+        '/api/studycontests/add',
+        registrationData,
+      )
+      console.log('등록 성공:', response.data)
+      navigate('/')
     } catch (error) {
-      console.error('등록 오류:', error);
+      console.error('등록 오류:', error)
     }
-  };
+  }
 
   return (
     <Container maxWidth="md" sx={{ mt: 4 }}>
       <h2>모집 글 작성 (스터디 / 공모 및 대회)</h2>
       <form onSubmit={handleSubmit} style={styles.form}>
-        
         <TextField
           label="제목"
           variant="outlined"
           fullWidth
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={e => setTitle(e.target.value)}
           required
           sx={{ mb: 3 }}
         />
@@ -72,14 +84,14 @@ function NewPostPage() {
           rows={4}
           fullWidth
           value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          onChange={e => setDescription(e.target.value)}
           required
           sx={{ mb: 3 }}
         />
 
         <Grid container spacing={2} sx={{ mb: 3 }}>
           <Grid item xs={6}>
-            <TextField
+            {/* <TextField
               label="모집 마감일"
               type="date"
               fullWidth
@@ -89,7 +101,16 @@ function NewPostPage() {
               value={deadline}
               onChange={(e) => setDeadline(e.target.value)}
               required
-            />
+            /> */}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['DateTimePicker']}>
+                <DateTimePicker
+                  label="모집 마감일"
+                  value={deadline}
+                  onChange={handleDeadlineChange}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
           </Grid>
 
           <Grid item xs={6}>
@@ -99,7 +120,7 @@ function NewPostPage() {
               fullWidth
               inputProps={{ min: 1 }}
               value={maxParticipants}
-              onChange={(e) => setMaxParticipants(e.target.value)}
+              onChange={e => setMaxParticipants(e.target.value)}
               required
             />
           </Grid>
@@ -109,11 +130,19 @@ function NewPostPage() {
           <FormLabel component="legend">모임 목적</FormLabel>
           <RadioGroup
             value={purpose}
-            onChange={(e) => setPurpose(e.target.value)}
+            onChange={e => setPurpose(e.target.value)}
             row
           >
-            <FormControlLabel value="study" control={<Radio />} label="스터디" />
-            <FormControlLabel value="contest" control={<Radio />} label="공모 및 대회" />
+            <FormControlLabel
+              value="study"
+              control={<Radio />}
+              label="스터디"
+            />
+            <FormControlLabel
+              value="contest"
+              control={<Radio />}
+              label="공모 및 대회"
+            />
           </RadioGroup>
         </FormControl>
 
@@ -121,7 +150,7 @@ function NewPostPage() {
           <FormLabel>모임 방식</FormLabel>
           <Select
             value={meetingType}
-            onChange={(e) => setMeetingType(e.target.value)}
+            onChange={e => setMeetingType(e.target.value)}
             required
           >
             <MenuItem value="온라인">온라인</MenuItem>
@@ -148,7 +177,7 @@ function NewPostPage() {
         </Button>
       </form>
     </Container>
-  );
+  )
 }
 
 const styles = {
@@ -157,6 +186,6 @@ const styles = {
     flexDirection: 'column',
     gap: '20px',
   },
-};
+}
 
-export default Auth(NewPostPage, true);
+export default Auth(NewPostPage, true)
--- src/components/views/NewPostPage/NewPost_mok.js
+++ src/components/views/NewPostPage/NewPost_mok.js
@@ -1,78 +1,88 @@
-import React, { useState } from 'react';
-import { TextField, Button, Box, Typography } from '@mui/material';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import axios from 'axios'; // axios import 추가
-import Auth from "../../../hoc/auth";
+import React, { useState } from 'react'
+import { TextField, Button, Box, Typography } from '@mui/material'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+import Auth from '../../../hoc/auth'
+
+import dayjs from 'dayjs'
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker'
 
 function NewPostPagemok() {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
-  const [location, setLocation] = useState('');
-  const [position, setPosition] = useState({ lat: 37.5665, lng: 126.9780 });
-  const [maxParticipants, setMaxParticipants] = useState(1);
-  const [searchAddress, setSearchAddress] = useState('');
-  const [keyword, setKeyword] = useState('');
-  const [markers, setMarkers] = useState([]);
-  const [selectedMarker, setSelectedMarker] = useState(null);
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [date, setdate] = useState(dayjs(''))
+  const [location, setLocation] = useState('')
+  const [position, setPosition] = useState({ lat: 37.5665, lng: 126.978 })
+  const [maxParticipants, setMaxParticipants] = useState(1)
+  const [searchAddress, setSearchAddress] = useState('')
+  const [keyword, setKeyword] = useState('')
+  const [markers, setMarkers] = useState([])
+  const [selectedMarker, setSelectedMarker] = useState(null)
 
-  const navigate = useNavigate();
-  const user = useSelector(state => state.user);
-
-  const handleMapClick = (_, mouseEvent) => {
-    const latlng = mouseEvent.latLng;
-    const lat = latlng.getLat();
-    const lng = latlng.getLng();
-    setPosition({ lat, lng });
-    fetchAddress(lat, lng);
+  const navigate = useNavigate()
+  const user = useSelector(state => state.user)
+  
+  const handleDateChange = (newValue) => {
+    setdate(newValue); // DateTimePicker에서 선택된 값을 상태에 저장
   };
 
+  const handleMapClick = (_, mouseEvent) => {
+    const latlng = mouseEvent.latLng
+    const lat = latlng.getLat()
+    const lng = latlng.getLng()
+    setPosition({ lat, lng })
+    fetchAddress(lat, lng)
+  }
+
   const fetchAddress = async (lat, lng) => {
-    const kakao = window.kakao;
-    const geocoder = new kakao.maps.services.Geocoder();
-    const coord = new kakao.maps.LatLng(lat, lng);
+    const kakao = window.kakao
+    const geocoder = new kakao.maps.services.Geocoder()
+    const coord = new kakao.maps.LatLng(lat, lng)
 
     geocoder.coord2Address(coord.getLng(), coord.getLat(), (result, status) => {
       if (status === kakao.maps.services.Status.OK) {
-        const address = result[0].address.address_name;
-        const roadAddress = result[0].road_address?.address_name || '';
-        const fullAddress = roadAddress ? roadAddress : address;
-        setLocation(fullAddress);
+        const address = result[0].address.address_name
+        const roadAddress = result[0].road_address?.address_name || ''
+        const fullAddress = roadAddress ? roadAddress : address
+        setLocation(fullAddress)
       } else {
-        setLocation('주소를 찾을 수 없습니다.');
+        setLocation('주소를 찾을 수 없습니다.')
       }
-    });
-  };
+    })
+  }
 
-  const handleSearchChange = (e) => {
-    setSearchAddress(e.target.value);
-  };
+  const handleSearchChange = e => {
+    setSearchAddress(e.target.value)
+  }
 
-  const handleKeywordChange = (e) => {
-    setKeyword(e.target.value);
-  };
+  const handleKeywordChange = e => {
+    setKeyword(e.target.value)
+  }
 
-  const handleSearchSubmit = (e) => {
-    e.preventDefault();
-    const kakao = window.kakao;
-    const geocoder = new kakao.maps.services.Geocoder();
+  const handleSearchSubmit = e => {
+    e.preventDefault()
+    const kakao = window.kakao
+    const geocoder = new kakao.maps.services.Geocoder()
 
     geocoder.addressSearch(searchAddress, (result, status) => {
       if (status === kakao.maps.services.Status.OK) {
-        const { x, y } = result[0];
-        setPosition({ lat: y, lng: x });
-        fetchAddress(y, x);
+        const { x, y } = result[0]
+        setPosition({ lat: y, lng: x })
+        fetchAddress(y, x)
       } else {
-        setLocation('주소를 찾을 수 없습니다.');
+        setLocation('주소를 찾을 수 없습니다.')
       }
-    });
-  };
+    })
+  }
 
   const handleKeywordSearch = () => {
-    const kakao = window.kakao;
-    const ps = new kakao.maps.services.Places();
+    const kakao = window.kakao
+    const ps = new kakao.maps.services.Places()
 
     ps.keywordSearch(keyword, (data, status) => {
       if (status === kakao.maps.services.Status.OK) {
@@ -80,90 +90,176 @@ function NewPostPagemok() {
           lat: item.y,
           lng: item.x,
           title: item.place_name,
-        }));
-        setMarkers(newMarkers);
-        setPosition({ lat: newMarkers[0].lat, lng: newMarkers[0].lng });
-        fetchAddress(newMarkers[0].lat, newMarkers[0].lng);
+        }))
+        setMarkers(newMarkers)
+        setPosition({ lat: newMarkers[0].lat, lng: newMarkers[0].lng })
+        fetchAddress(newMarkers[0].lat, newMarkers[0].lng)
       } else {
-        setLocation('키워드로 장소를 찾을 수 없습니다.');
+        setLocation('키워드로 장소를 찾을 수 없습니다.')
       }
-    });
-  };
+    })
+  }
 
-  const handleMarkerClick = (marker) => {
-    setSelectedMarker(marker);
-  };
+  const handleMarkerClick = marker => {
+    setSelectedMarker(marker)
+  }
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async event => {
+    event.preventDefault()
     const registrationData = {
       title,
       writer: user.userData._id, // redux에서 userData 가져옴
       content: description,
-      date,
       location,
       maximumNum: maxParticipants,
-      datetime: new Date(date), // 날짜 형식 변환
+      datetime: date.toISOString(), // dayjs를 사용하여 ISO 형식으로 변환
       type: 'mogako',
       joinedUser: [],
       lat: position.lat,
       lng: position.lng,
-    };
+    }
 
     try {
       // 서버로 POST 요청
-      const response = await axios.post('/api/mogakos/add', registrationData);
-      console.log('등록 성공:', response.data);
+      const response = await axios.post('/api/mogakos/add', registrationData)
+      console.log('등록 성공:', response.data)
 
       // 성공 시 메인 페이지로 이동
-      navigate('/');
+      navigate('/')
     } catch (error) {
-      console.error('등록 오류:', error);
+      console.error('등록 오류:', error)
     }
-  };
+  }
 
   return (
     <Box sx={styles.container}>
-      <Typography variant="h4" gutterBottom sx={{ textAlign: 'left', marginBottom: '20px', fontWeight: 'bold', fontSize: '24px' }}>
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{
+          textAlign: 'left',
+          marginBottom: '20px',
+          fontWeight: 'bold',
+          fontSize: '24px',
+        }}
+      >
         모집 글 작성 ( 모각코 )
       </Typography>
       <Box component="form" onSubmit={handleSubmit} sx={styles.form}>
-        <TextField label="제목" value={title} onChange={(e) => setTitle(e.target.value)} required fullWidth />
-        <TextField label="내용" value={description} onChange={(e) => setDescription(e.target.value)} required multiline rows={4} fullWidth />
+        <TextField
+          label="제목"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          required
+          fullWidth
+        />
+        <TextField
+          label="내용"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          required
+          multiline
+          rows={4}
+          fullWidth
+        />
         <Box sx={{ display: 'flex', gap: '20px', mb: 0 }}>
-          <TextField label="날짜" type="date" value={date} onChange={(e) => setDate(e.target.value)} required InputLabelProps={{ shrink: true }} sx={styles.smallInput} />
-          <TextField label="장소" value={location} onChange={(e) => setLocation(e.target.value)} required sx={styles.smallInput} />
+          {/* <TextField label="날짜" type="date" value={date} onChange={(e) => setDate(e.target.value)} required InputLabelProps={{ shrink: true }} sx={styles.smallInput} /> */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DateTimePicker']}>
+              <DateTimePicker label="날짜, 시간 지정" value={date} onChange={handleDateChange} />
+            </DemoContainer>
+          </LocalizationProvider>
+          <TextField
+            label="장소"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            required
+            sx={styles.smallInput}
+          />
         </Box>
 
-        <Box component="form" onSubmit={handleSearchSubmit} sx={styles.searchBox}>
-          <TextField label="주소 검색 (도로명 주소)" value={searchAddress} onChange={handleSearchChange} required sx={styles.searchInput} />
-          <Button type="button" onClick={handleSearchSubmit} variant="contained" color="primary" sx={styles.searchButton}>
+        <Box
+          component="form"
+          onSubmit={handleSearchSubmit}
+          sx={styles.searchBox}
+        >
+          <TextField
+            label="주소 검색 (도로명 주소)"
+            value={searchAddress}
+            onChange={handleSearchChange}
+            required
+            sx={styles.searchInput}
+          />
+          <Button
+            type="button"
+            onClick={handleSearchSubmit}
+            variant="contained"
+            color="primary"
+            sx={styles.searchButton}
+          >
             검색
           </Button>
         </Box>
 
         <Box component="form" sx={styles.keywordBox}>
-          <TextField label="장소 검색 (예: 수서역 카페)" value={keyword} onChange={handleKeywordChange} required sx={styles.searchInput} />
-          <Button type="button" onClick={handleKeywordSearch} variant="contained" color="primary" sx={styles.searchButton}>
+          <TextField
+            label="장소 검색 (예: 수서역 카페)"
+            value={keyword}
+            onChange={handleKeywordChange}
+            required
+            sx={styles.searchInput}
+          />
+          <Button
+            type="button"
+            onClick={handleKeywordSearch}
+            variant="contained"
+            color="primary"
+            sx={styles.searchButton}
+          >
             키워드 검색
           </Button>
         </Box>
 
-        <Map center={position} style={{ width: '100%', height: '350px', marginBottom: '20px' }} level={3} onClick={handleMapClick}>
-          {markers.map((marker, index) => (
-            (selectedMarker === null || selectedMarker.title === marker.title) && (
-              <MapMarker key={index} position={{ lat: marker.lat, lng: marker.lng }} title={marker.title} onClick={() => handleMarkerClick(marker)} />
-            )
-          ))}
+        <Map
+          center={position}
+          style={{ width: '100%', height: '350px', marginBottom: '20px' }}
+          level={3}
+          onClick={handleMapClick}
+        >
+          {markers.map(
+            (marker, index) =>
+              (selectedMarker === null ||
+                selectedMarker.title === marker.title) && (
+                <MapMarker
+                  key={index}
+                  position={{ lat: marker.lat, lng: marker.lng }}
+                  title={marker.title}
+                  onClick={() => handleMarkerClick(marker)}
+                />
+              ),
+          )}
         </Map>
 
-        <TextField label="모집 인원 수" type="number" value={maxParticipants} onChange={(e) => setMaxParticipants(e.target.value)} required InputProps={{ inputProps: { min: 1 } }} sx={styles.smallInput} />
-        <Button type="submit" variant="contained" color="primary" sx={styles.submitButton}>
+        <TextField
+          label="모집 인원 수"
+          type="number"
+          value={maxParticipants}
+          onChange={e => setMaxParticipants(e.target.value)}
+          required
+          InputProps={{ inputProps: { min: 1 } }}
+          sx={styles.smallInput}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={styles.submitButton}
+        >
           등록
         </Button>
       </Box>
     </Box>
-  );
+  )
 }
 
 const styles = {
@@ -215,6 +311,6 @@ const styles = {
     margin: '20px auto',
     '&:hover': { backgroundColor: '#d40070' },
   },
-};
+}
 
-export default Auth(NewPostPagemok, true);
+export default Auth(NewPostPagemok, true)
