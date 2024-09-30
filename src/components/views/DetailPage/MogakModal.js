51a1fe610b7d95fa2ae4466a31ee1b88a9bdc8d9
kimdayeon37 <kdy37912@naver.com>
2024-09-30 15:11:31 +0900

    feat: 게시글 수정 title

---
+++ src/components/views/DetailPage/MogakModal.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react'
+import {
+    TextField,
+    Modal,
+    Button,
+    Radio,
+    RadioGroup,
+    FormControlLabel,
+    FormControl,
+    FormLabel,
+    Select,
+    MenuItem,
+    Box,
+  } from '@mui/material'
+import { secondary_color } from '../../constants/colors';
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { updateMogakoPost } from '../../_actions/post_action';
+import { useDispatch } from 'react-redux';
+
+function MogakModal({ open, handleClose }) {
+    const dispatch = useDispatch();
+
+    const { type, postId } = useParams()
+    const [post, setPost] = useState({})
+    const [editedTitle, setEditedTitle] = useState('')
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = type === 'mogako'
+              ? await axios.get(`/api/mogakos/${postId}`)
+              : await axios.get(`/api/studyContests/${postId}`);
+            setPost(response.data);
+            setEditedTitle(response.data.title); // 가져온 데이터를 수정 가능한 상태로 설정
+          } catch (error) {
+            console.log(error);
+          }
+        };
+    
+        fetchData();
+      }, [type, postId]);
+    
+
+      const onClickUpdate = () => {
+        const body = {
+          postId: postId,
+          title: editedTitle,
+        };
+        dispatch(updateMogakoPost(body)).then(res => {
+          if (res.payload.success) {
+            handleClose();
+        } else {
+            alert('글 수정에 실패했습니다.');
+          }
+        });
+      };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+      <TextField
+          label="제목"
+          variant="outlined"
+          fullWidth
+          value={editedTitle}
+          onChange={e => setEditedTitle(e.target.value)}
+          required
+          sx={{ mb: 3 }}
+        />
+        <Button
+          type="submit"
+          onClick={onClickUpdate} // 이 부분에서 전달된 onSubmit 함수 호출
+          variant="contained"
+          sx={{
+            borderRadius: 2,
+            backgroundColor: secondary_color,
+            fontWeight: 700,
+            display: 'block',
+            margin: '20px auto',
+          }}
+        >
+          확인
+        </Button>
+      </Box>
+    </Modal>
+  );
+}
+
+export default MogakModal;
--- src/components/views/DetailPage/PostUpdateDelete.js
+++ src/components/views/DetailPage/PostUpdateDelete.js
@@ -1,63 +1,38 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import { Button } from '@mui/material'
 import { secondary_color } from '../../constants/colors'
 import { updateMogakoPost, deletePost } from '../../_actions/post_action'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import MogakModal from './MogakModal'
 
 function PostUpdateDelete(props) {
-  // props.postType == 'mogako'
   const dispatch = useDispatch()
   const navigate = useNavigate()
-  const [isEditing, setIsEditing] = useState(false);
-  const [myLists, setMyLists] = useState([])
+  const [open, setOpen] = useState(false)
 
-//   useEffect(() => {
-//       axios.get('/api/posts/myposts')
-//       .then(response => {
-//         console.log(response.data);
-//         if (response.data) {
-//           setMyLists(response.data);
-//         }
-//       })
-//       .catch(err => {
-//         console.error('Error fetching posts:', err);
-//       });
-//   })
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
-  const onClickUpdate = () => {
-    const body = {
-        postId: props.postId,
-        postType: props.type
-    }
-
-    dispatch(updateMogakoPost(body)).then(res => {
-      if (res.payload.success) {
-        setIsEditing(false);
-      } else {
-        alert('글 수정에 실패했습니다.')
-      }
-    })
-  }
   const onClickDelete = () => {
     const body = {
-        postId: props.postId,
-        postType: props.type
+      postId: props.postId,
+      postType: props.type,
     }
-
     dispatch(deletePost(body)).then(res => {
       if (res.payload.success) {
-          alert('글 삭제에 성공했습니다.')
-          navigate('/mypage')
+        alert('글 삭제에 성공했습니다.')
+        navigate('/mypage')
       } else {
         alert('글 삭제에 실패했습니다.')
       }
     })
   }
+
   return (
     <>
       <Button
-        onClick={onClickUpdate}
+        onClick={handleOpen}
         variant="contained"
         sx={{
           borderRadius: 2,
@@ -80,6 +55,8 @@ function PostUpdateDelete(props) {
       >
         삭제
       </Button>
+
+      <MogakModal open={open} handleClose={handleClose} />
     </>
   )
 }
